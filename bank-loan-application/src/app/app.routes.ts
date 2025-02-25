4d5cf758c079d1c6dd39367e4e3aeaa2a914a85a
Bhavisha Dashrathe <97605608+bhavisha-6573@users.noreply.github.com>
2025-02-25 10:20:41 +0530

    Update app.routes.ts routes created

--- bank-loan-application/src/app/app.routes.ts
+++ bank-loan-application/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'newApplication',
+    pathMatch: 'full'
+  },
+  {
+    path: 'newApplication',
+    component: LoanApplicationComponent
+  },
+  {
+    path: 'applicationList',
+    component: ApplicationListComponent 
+  }
+];

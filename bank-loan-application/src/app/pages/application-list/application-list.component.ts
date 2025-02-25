f2effb570319edc1ab264096cc5e3220c72d6e63
Bhavisha Dashrathe <97605608+bhavisha-6573@users.noreply.github.com>
2025-02-25 10:21:53 +0530

    Update application-list.component.ts

--- bank-loan-application/src/app/pages/application-list/application-list.component.ts
+++ bank-loan-application/src/app/pages/application-list/application-list.component.ts
@@ -1,1 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-application-list',
+  templateUrl: './application-list.component.html',
+  styleUrls: ['./application-list.component.css']
+})
+export class ApplicationListComponent {
+  
+}
 

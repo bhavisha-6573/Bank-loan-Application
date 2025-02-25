e12ce4b237cc49680cfd7b237f1750b9f0026fa2
Bhavisha Dashrathe <97605608+bhavisha-6573@users.noreply.github.com>
2025-02-25 10:17:03 +0530

    Update loan-application.component.ts

--- bank-loan-application/src/app/pages/loan-application/loan-application.component.ts
+++ bank-loan-application/src/app/pages/loan-application/loan-application.component.ts
@@ -1,1 +1,13 @@
+// loan-application.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-application',
+  templateUrl: './loan-application.component.html',
+  styleUrls: ['./loan-application.component.css']
+})
+export class LoanApplicationComponent {
+ 
+}
 
